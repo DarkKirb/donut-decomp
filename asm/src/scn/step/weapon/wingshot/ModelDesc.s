559dda1f939fdbed73a98d8e4cb8a501e23f8923
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-02-20 15:21:06 +0100

    Merge scn::step::weapon::wingshot

--- asm/src/scn/step/weapon/wingshot/ModelDesc.s
+++ asm/src/scn/step/weapon/wingshot/ModelDesc.s
@@ -129,3 +129,11 @@ lbl_803E5BCC:
 
 	.4byte 0x546F704C
 	.4byte 0
+.include "macros.inc"
+
+.section .sdata2, "wa"  # 0x8055DF80 - 0x805643C0 ; 0x00006440
+.global "T_ANIM_ENTRIES__Q53scn4step6weapon8wingshot23@unnamed@ModelDesc_cpp@"
+"T_ANIM_ENTRIES__Q53scn4step6weapon8wingshot23@unnamed@ModelDesc_cpp@":
+
+	.4byte "@50124_8055C1CC"
+	.4byte 0
--- asm/src/scn/step/weapon/wingshot/StateMain.s
+++ asm/src/scn/step/weapon/wingshot/StateMain.s
@@ -518,3 +518,61 @@ __vt__Q53scn4step6weapon13wingshockwave9StateMain:
 "@56174_8055C354":
 
 	.4byte 0x0000001D
+.include "macros.inc"
+
+.section .sdata2, "wa"  # 0x8055DF80 - 0x805643C0 ; 0x00006440
+.global "@56698_80563F80"
+"@56698_80563F80":
+
+	.4byte 0
+
+.global "@56699_80563F84"
+"@56699_80563F84":
+
+	.4byte 0x3E4CCCCD
+
+.global "@56700"
+"@56700":
+
+	.4byte 0xBF800000
+
+.global "@56701_80563F8C"
+"@56701_80563F8C":
+
+	.4byte 0x3FB40000
+
+.global "@56702_80563F90"
+"@56702_80563F90":
+
+	.4byte 0x42B40000
+
+.global "@56716_80563F94"
+"@56716_80563F94":
+
+	.4byte 0x42AA0000
+
+.global "@56724_80563F98"
+"@56724_80563F98":
+
+	.4byte 0x3F800000
+
+.global "@56725_80563F9C"
+"@56725_80563F9C":
+
+	.4byte 0
+
+.global "@56726_80563FA0"
+"@56726_80563FA0":
+
+	.4byte 0x3E99999A
+
+.global "@56744"
+"@56744":
+
+	.4byte 0xBE4CCCCD
+
+.global "@56765_80563FA8"
+"@56765_80563FA8":
+
+	.4byte 0x3F400000
+	.4byte 0

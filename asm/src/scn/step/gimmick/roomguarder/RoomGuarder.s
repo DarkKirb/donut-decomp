01be6fbcb30dc099e969e4ccef7508d4bfd68602
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-02-20 13:30:30 +0100

    Merge scn::step::gimmick::roomguarder

--- asm/src/scn/step/gimmick/roomguarder/RoomGuarder.s
+++ asm/src/scn/step/gimmick/roomguarder/RoomGuarder.s
@@ -3315,3 +3315,55 @@ lbl_8031E070:
 	.4byte 0x2F43616D
 	.4byte 0x6572614C
 	.4byte 0x6F636B00
+.include "macros.inc"
+
+.section .sdata2, "wa"  # 0x8055DF80 - 0x805643C0 ; 0x00006440
+.global "@63448"
+"@63448":
+
+	.4byte 0
+
+.global "@63691"
+"@63691":
+
+	.4byte 0xFF7FFFFF
+
+.global "@63894"
+"@63894":
+
+	.4byte 0x3F000000
+
+.global "@63895"
+"@63895":
+
+	.4byte 0x3ECCCCCD
+
+.global "@63956"
+"@63956":
+
+	.4byte 0x40000000
+
+.global "@63957"
+"@63957":
+
+	.4byte 0x3F800000
+
+.global "@60428"
+"@60428":
+
+	.4byte 0xFFFFFFF0
+
+.global "@64155"
+"@64155":
+
+	.4byte 0xBF000000
+
+.global "@64156"
+"@64156":
+
+	.4byte 0x3DCCCCCD
+
+.global "@64195"
+"@64195":
+
+	.4byte 0x3727C5AC
--- obj_files.mk
+++ obj_files.mk
@@ -2379,9 +2379,10 @@ $(BUILD_DIR)/asm/src/scn/step/gimmick/otachidai/Otachidai.o \
 \
 $(BUILD_DIR)/asm/src/scn/step/gimmick/rollball/Manager.o \
 \
+$(BUILD_DIR)/asm/src/scn/step/gimmick/roomguarder/RoomGuarder.o \
+\
 $(BUILD_DIR)/asm/Linker/Generated/Symbol/File.o \
 \
-$(BUILD_DIR)/asm/src/scn/step/gimmick/roomguarder/RoomGuarder_8031b40c.o \
 $(BUILD_DIR)/asm/src/scn/step/gimmick/roomguardermarker/Manager_8031e08c.o \
 $(BUILD_DIR)/asm/src/scn/step/gimmick/roomguardermarker/RoomGuarderMarker_8031e360.o \
 $(BUILD_DIR)/asm/src/scn/step/gimmick/shipevent/AddOnMint_8031e3e8.o \
@@ -3100,7 +3101,6 @@ $(BUILD_DIR)/asm/src/util/StateChanger_80405cd0.o \
 $(BUILD_DIR)/asm/src/util/Thread_80405f14.o \
 $(BUILD_DIR)/asm/src/util/UIntAABBox_8040603c.o \
 \
-$(BUILD_DIR)/asm/src/scn/step/gimmick/roomguarder/RoomGuarder_805629f8.o \
 $(BUILD_DIR)/asm/src/scn/step/gimmick/shipevent/ShipEvent_80562a20.o \
 $(BUILD_DIR)/asm/src/scn/step/gimmick/shutter/Shutter_80562a38.o \
 $(BUILD_DIR)/asm/src/scn/step/gimmick/stake/Stake_80562a78.o \
